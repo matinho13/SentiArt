949f3bdf53ed4d6d9e037c3b4270de5bb05570ef
matinho13 <ajacobs@zedat.fu-berlin.de>
2020-11-17 08:21:22 +0100

    Update SentiArt20.py

--- SentiArt20.py
+++ SentiArt20.py
@@ -16,11 +16,11 @@ fn = 'momo_4s.txt'
 with codecs.open(fn,'r','utf-8') as f:
     raw = f.read()
 
-# get the table with sentiment values based on a vector space model (w2v, skipgram, 300d) and the label list published in:
-# https://www.frontiersin.org/articles/10.3389/fnhum.2017.00622/full#supplementary-material
+# get the table with sentiment values. they are based on a vector space model (w2v, skipgram, 300d) and the label list published in:
+# https://www.frontiersin.org/articles/10.3389/fnhum.2017.00622/full#supplementary-material; https://www.frontiersin.org/articles/10.3389/fpsyg.2020.574746/full
 # the values for each word are: AAPz,ang_z,fear_z,disg_z,hap_z,sad_z,surp_z
-# they provide the affective-aesthetic potential (AAP) and discrete emotion values (anger, fear, disgust, sadness and surprise) 
-# for each word based on their semantic relatedness (as computed by w2v) with labels (semantic anchors) described in the publications mentioned in readme.md
+# they provide the affective-aesthetic potential (AAP) and discrete emotion values (anger, fear, disgust, sadness and surprise), all standardized (z-values), 
+# for each word, based on their semantic relatedness (as computed by w2v) with labels (semantic anchors) described in the publications mentioned in readme.md
 
 TC = '120kSentiArt_DE.xlsx' # for German texts
 #TC = '250kSentiArt_EN.xlsx' # for English texts
